32b98df51a69fd2866d35a8f5a0e3ceb9b7be11e
Narava Rupa Sri <n.rupasri25@gmail.com>
2025-07-28 19:07:01 +0530

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -1,14 +1,17 @@
 import unittest
 import medical_data_visualizer
 
+
 class MedicalDataVisualizerTestCase(unittest.TestCase):
     def test_cat_plot(self):
         fig = medical_data_visualizer.draw_cat_plot()
-        self.assertIsNotNone(fig, "Expected cat plot figure to be returned.")
+        self.assertIsNotNone(fig)
 
     def test_heat_map(self):
         fig = medical_data_visualizer.draw_heat_map()
+        self.assertIsNotNone(fig)
         self.assertEqual(len(fig.axes[0].images), 1, "Expected one heatmap image on the axis.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
