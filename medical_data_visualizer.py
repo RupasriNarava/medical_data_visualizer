ac49f6c75a9ab3a9a649c70994a63b06db23ed2c
Narava Rupa Sri <n.rupasri25@gmail.com>
2025-07-28 19:07:58 +0530

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,32 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = pd.read_csv('medical_examination.csv')
+df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
 df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2)).apply(lambda x: 1 if x > 25 else 0)
 
-# Normalize cholesterol and glucose
-df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
-df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
+# Normalize data by making 0 good and 1 bad
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # Categorical plot
 def draw_cat_plot():
-    df_cat = pd.melt(df,
-                     id_vars='cardio',
-                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=[
+        'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
 
-    fig = sns.catplot(data=df_cat,
-                      x='variable',
-                      y='total',
-                      hue='value',
-                      col='cardio',
-                      kind='bar').fig
+    fig = sns.catplot(data=df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar').fig
     return fig
 
-# Heat map
+# Heatmap
 def draw_heat_map():
     df_heat = df[
         (df['ap_lo'] <= df['ap_hi']) &
@@ -39,17 +33,13 @@ def draw_heat_map():
         (df['weight'] <= df['weight'].quantile(0.975))
     ]
 
-    corr = df_heat.corr()
+    corr = df_heat.corr(numeric_only=True)
     mask = np.triu(np.ones_like(corr, dtype=bool))
 
     fig, ax = plt.subplots(figsize=(12, 10))
-    sns.heatmap(corr,
-                mask=mask,
-                annot=True,
-                fmt='.1f',
-                square=True,
-                linewidths=0.5,
-                cbar_kws={'shrink': 0.5},
-                ax=ax)
-    return fig
+    sns.heatmap(
+        corr, mask=mask, annot=True, fmt='.1f', center=0, square=True,
+        linewidths=0.5, cbar_kws={"shrink": 0.5}, ax=ax
+    )
 
+    return fig
