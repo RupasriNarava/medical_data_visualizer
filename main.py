833bf7769e2b9c5a55ebf0b1b9d00aa3d97a0d25
Narava Rupa Sri <n.rupasri25@gmail.com>
2025-07-28 18:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# This file is used to run your functions and test them visually
 from medical_data_visualizer import draw_cat_plot, draw_heat_map
 
-# Run the categorical plot function and save image
-cat_plot_fig = draw_cat_plot()
-cat_plot_fig.savefig("catplot.png")
+# Generate and save plots
+fig1 = draw_cat_plot()
+fig1.savefig("catplot.png")
 
-# Run the heatmap function and save image
-heat_map_fig = draw_heat_map()
-heat_map_fig.savefig("heatmap.png")
+fig2 = draw_heat_map()
+fig2.savefig("heatmap.png")
