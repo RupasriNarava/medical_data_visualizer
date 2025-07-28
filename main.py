d3b9259d385eb8e424a35427d98769b8baa75b38
Narava Rupa Sri <n.rupasri25@gmail.com>
2025-07-28 19:07:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from medical_data_visualizer import draw_cat_plot, draw_heat_map
 
-# Generate and save plots
-fig1 = draw_cat_plot()
-fig1.savefig("catplot.png")
-
-fig2 = draw_heat_map()
-fig2.savefig("heatmap.png")
+# Save plots
+draw_cat_plot().savefig("catplot.png")
+draw_heat_map().savefig("heatmap.png")
